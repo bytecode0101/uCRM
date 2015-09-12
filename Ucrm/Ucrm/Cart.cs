04daf77b1342b6a5d94564175b4527b5949613c0
andrei patrascu <Aluvspam@yahoo.com>
2015-09-12 12:03:44 +0300

    Added cart, order andcategory classes

---
+++ Ucrm/Ucrm/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucrm
+{
+    class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        //public Dictionary<Product, int>  Items { get; set; }
+    }
+}
