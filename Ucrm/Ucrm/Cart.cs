710caa45dafebb4536cefb8a6862fe4efb5b6ba6
andrei patrascu <Aluvspam@yahoo.com>
2015-09-13 12:24:05 +0300

    small update to Cart /Order classes

--- Ucrm/Ucrm/Cart.cs
+++ Ucrm/Ucrm/Cart.cs
@@ -10,6 +10,14 @@ namespace Ucrm
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        //public Dictionary<Product, int>  Items { get; set; }
+        public Dictionary <Product, int>  Items { get; set; }
+
+        public Cart( Guid _UserId)
+        {
+            this.Id = Guid.NewGuid();
+            this.UserId = _UserId;
+            this.Items = new Dictionary<Product, int> { };
+        }
+
     }
 }
--- Ucrm/Ucrm/Order.cs
+++ Ucrm/Ucrm/Order.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Ucrm
 {
-    class Order
+    class Order : Cart
     {
+        public DateTime SetTime { get; set; }
+        public DateTime ConfirmedTime { get; set; }
+        public OrderStatus Status { get; set; }
+        public Payment PayMethod { get; set; }
+        public Delivery Delivery { get; set; }
+        public DateTime DeliveryTime { get; set; }
+
+        public Order (Guid _UserId) : base (_UserId)
+        {
+            
+
+        }
     }
+
 }
