0abc6ed0c5ece7a0d59c009e91bbf8a130e4ebcf
MariaDucu <mari.ducu@yahoo.com>
2015-09-15 19:25:37 +0300

    Subcategory class added.

---
+++ Ucrm/Ucrm/SubCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucrm
+{
+    class SubCategory
+    {
+        public Guid id { get; set; }
+        public string NameSubCat { get; set; }
+    }
+}
