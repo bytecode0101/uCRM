7f827af4c23b4ef4349390cd1fb8b408cdc4a313
NicoletaStroescu <nicoleta.stroescu@outlook.com>
2015-09-15 20:23:47 +0300

    Added stock methods

--- Ucrm/Ucrm/Stock.cs
+++ Ucrm/Ucrm/Stock.cs
@@ -1,17 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
 
 namespace Ucrm
 {
+    ///This is the stock of our products. 
+    ///This will keep all of our products with the corresponding quantity
+    ///
     class Stock
     {
-        ///This is the stock of our products. 
-        ///This will keep all of our products with the corresponding quantity
-        ///
+        Dictionary<Guid, int> stock;
+        ///constructor
+        public Stock() 
+        {
+            stock = new Dictionary<Guid, int>();
+        }
+        /// <summary>
+        /// This will add a new Product to our sstock
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="quantity"></param>
+        private void AddNewProduct (Guid p,int quantity)
+        {
+            stock.Add(p, quantity);
+        }
+        /// <summary>
+        /// This will add a new quantity to a specific product
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="quantity"></param>
+        private void AddQuantity(Guid p, int quantity)
+        {
+            if (!stock.ContainsKey(p))
+            {
+                AddNewProduct(p, quantity);
+            }
+            else
+            {
+                stock[p]+=quantity;
+            }
+                
 
+                
+        }
 
     }
 }
