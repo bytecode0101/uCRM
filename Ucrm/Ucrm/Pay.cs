1a9bb81ddb99bdfee694992e31049a7f20212f7e
Ilie Cristian Marius <ilie_cristian_marius@hotmail.com>
2015-09-15 18:43:36 +0300

    add Pay Methods + Remove from cart

---
+++ Ucrm/Ucrm/Pay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucrm
+{
+    public class Pay
+    {
+        public void CheckPaymentMethod()
+        {
+            //if cash
+            //if banca
+            //if op
+        }
+
+        public bool CheckPaymentStatus()
+        {
+            //if ()
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return true;
+        }
+
+
+        public void RegisterPayment()
+        {
+            //inreg plata in system.. daca avem alocata o baza de data sau inregistrari particularizate pentru clienti PJ
+        }
+
+
+    }
+}
