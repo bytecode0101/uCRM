00d4ae6f559b1e36fcde822a9b8ee78d39f2984f
Ilie Cristian Marius <ilie_cristian_marius@hotmail.com>
2015-09-15 20:52:16 +0300

    trying to get the latest version

--- Ucrm/Ucrm/Cart.cs
+++ Ucrm/Ucrm/Cart.cs
@@ -9,26 +9,28 @@ namespace Ucrm
         /// Cart class to generate Carts
         /// </summary>
 
-
+        public Guid ProdId { get; set; }
         public Guid Id { get; set; } //Cart ID
         public Guid UserId { get; set; } // ID of theuser who created the cart
-        public Dictionary <Product, int>  Items { get; set; } // List of Products and number of each Product
+        public Dictionary<Guid, int> Items { get; set; } // List of Products and number of each Product
 
-        public Cart( Guid _UserId)
+        public Cart(Guid _UserId)
         {
             this.Id = Guid.NewGuid();
             this.UserId = _UserId;
-            this.Items = new Dictionary<Product, int> { };
+            this.Items = new Dictionary<Guid, int> { };
         }
 
-       /// <summary>
-       /// The RemoveItemFromCart method deletes an item from cart at a certain position.
-       /// </summary>
-       /// <param name="prodlist">prodlist is a Dictionary parameter that allows us to delete a Dictionary entry</param>
-        public void RemoveItemFromCart(Dictionary<Product, int> prodlist)
+        /// <summary>
+        /// The RemoveItemFromCart method deletes an item from cart at a certain position.
+        /// </summary>
+        /// <param name="prodlist">prodlist is a Dictionary parameter that allows us to delete a Dictionary entry</param>
+        public void RemoveItemFromCart(Guid productId)
         {
-            prodlist.Remove(new Product());
-            //remove product from cart based on its id/name/its placement in the product List
+            if (Items.ContainsKey(productId))
+            {
+                Items.Remove(productId);
+            }
         }
     }
 }
