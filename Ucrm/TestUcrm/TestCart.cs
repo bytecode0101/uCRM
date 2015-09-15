66c2800578797d1a4f80cbbea0c5e8e38680828c
bytecode <bytecode0101@yahoo.com>
2015-09-15 21:02:22 +0300

    add TestCart.cs

---
+++ Ucrm/TestUcrm/TestCart.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using Ucrm;
+
+namespace TestUcrm
+{
+    [TestFixture]
+    public class TestCart
+    {
+        [Test]
+        public void TestAddProduct()
+        {
+            var userId = Guid.NewGuid();
+            Product p = new Product();
+            Cart cart = new Cart(userId);
+
+            cart.AddProduct(p, 10);
+
+            Assert.AreEqual(10, cart.Items[p.Id]);
+        }
+    }
+}
