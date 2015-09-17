d9568b269960e59e49a6c71903c33cd254b33472
sallysway <ioanaandreea476@gmail.com>
2015-09-17 08:05:58 -0700

    updated address with country method

---
+++ Ucrm/TestUcrm/TestCountry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ucrm;
+
+namespace TestUcrm
+{
+    [TestFixture]
+    public class TestCountry
+    {
+        [Test]
+        public string ChooseCountry()
+        {     
+
+        Address firstaddress = new Address();
+            firstaddress.SelectCountry();
+    }
+}
+}
