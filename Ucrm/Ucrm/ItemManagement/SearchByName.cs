88a9207de172fca98bed99f8315aaf41860f7f8b
sallysway <ioanaandreea476@gmail.com>
2015-10-10 02:02:10 -0700

    Updated ItemManagement

--- Ucrm/Ucrm/ItemManagement/SearchByName.cs
+++ Ucrm/Ucrm/ItemManagement/SearchByName.cs
@@ -8,8 +8,21 @@ namespace Ucrm
 {
     public class SearchByName : SearchEngine
     {
+        List<Product> product_list;
+        Product product;
         public override List<Product> Search(string S)
         {
+            if (S == product.Name)
+            {
+                product_list.Add(product);
+            }
+
+            if (product.Name.Contains(S))
+            {
+                product_list.Add(S);
+            }
+
+
         }
     }
 }
